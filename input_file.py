60a1182263335248e9f4b64c2591a2d46dce2a89
Ruth <shepardr25@up.edu>
2023-11-15 11:44:12 -0800

    Initial Commit

--- input_file.py
+++ input_file.py
@@ -1,13 +1,31 @@
+# input_file.py
 from syntaxChecker import syntaxChecker
+from scanner import Scanner
+from parser import Parser
+from engine import Engine
 
 if __name__ == "__main__":
-    test_regex = "A\d+"
-    test_string = "A123"
+    # Take user input for regex and test string
+    regex_input = input("Enter the regex: ")
+    test_string = input("Enter the test string: ")
 
-    # Syntax Checker
-    if syntaxChecker.is_valid(test_regex):
-        print(f"Regex: {test_regex}")
-        print(f"Input String: {test_string}")
-        print("Both regex and string are valid.")
+    # Syntax checking
+    syntax_checker = syntaxChecker(regex_input)
+    is_valid_regex = syntax_checker.is_valid_regex()
+
+    if is_valid_regex:
+        # Tokenization
+        scanner = Scanner(regex_input)
+        tokens = scanner.tokenize()
+
+        # Parsing
+        parser = Parser(tokens)
+        ast = parser.parse()
+
+        # Engine
+        regex_engine = Engine(ast)
+        result = regex_engine.match(test_string)
+
+        print(f"Regex matches the string: {result}")
     else:
         print("Invalid regex syntax.")
